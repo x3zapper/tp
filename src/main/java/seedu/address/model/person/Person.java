8825e693524853685a689edc7692fd2dc6b8c4c3
x3zapper <142211386+x3zapper@users.noreply.github.com>
2025-09-28 23:19:25 +0800

    Update Person to support Remark

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -24,17 +24,19 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
+    private final Remark remark;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Remark remark) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
+        this.remark = remark;
     }
 
     public Name getName() {
@@ -72,6 +74,11 @@ public class Person {
 
         return otherPerson != null
                 && otherPerson.getName().equals(getName());
+    }
+
+    /** Returns person remark data */
+    public Remark getRemark() {
+        return remark;
     }
 
     /**
