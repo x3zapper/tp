11756eaa3fb0ca150beea3006c1dd0aa62ae5233
x3zapper <142211386+x3zapper@users.noreply.github.com>
2025-09-28 23:04:29 +0800

    Update RemarkCommandParser

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -26,7 +26,9 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (IllegalValueException ive) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), ive);
+            throw new ParseException(String.format(
+                    MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), ive
+            );
         }
 
         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
