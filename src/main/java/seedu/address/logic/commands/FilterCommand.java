9bcbf0397313df9fc0ae7f67bdad82cd844cdd63
zheng hong <tewzhenghong@outlook.com>
2025-10-12 15:25:05 +0800

    Add FilterCommand class

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,6 +1,11 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
 import seedu.address.model.Model;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Finds and lists all persons in address book whose tags contains any of those in the argument.
@@ -10,9 +15,45 @@ public class FilterCommand extends Command {
 
     public static final String COMMAND_WORD = "filter";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose tags contain "
+            + "all of the specified tags (case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: TAG [MORE_TAGS]...\n"
+            + "Example: " + COMMAND_WORD + " client VIP";
+
+    private final TagContainsKeywordsPredicate predicate;
+
+    public FilterCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult("Filter testing");
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        FilterCommand otherFilterCommand = (FilterCommand) other;
+        return predicate.equals(otherFilterCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
     }
 }
 
---
+++ src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
@@ -0,0 +1,48 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import seedu.address.model.tag.Tag;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Person}'s {@code Tag}s match all of the specified keywords.
+ * Matching is case-sensitive.
+ */
+public class TagContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public TagContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        Set<String> personTagNames = person.getTags().stream()
+                .map(tag -> tag.tagName)
+                .collect(Collectors.toSet());
+
+        // Must contain ALL input tags (case-sensitive)
+        return keywords.stream().allMatch(personTagNames::contains);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof TagContainsKeywordsPredicate)) {
+            return false;
+        }
+        TagContainsKeywordsPredicate otherPredicate = (TagContainsKeywordsPredicate) other;
+        return keywords.equals(otherPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
