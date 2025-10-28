611826fa88ba64fece3cc8ec27077826864a733b
Edmund <l>
2025-10-28 13:17:49 +0800

    Add comments to cases as per PR suggestion

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -41,22 +41,31 @@ public class SortCommandParser {
         String sortType = argMultimap.getValue(PREFIX_SORT_TYPE).get();
         String sortOrder = argMultimap.getValue(PREFIX_SORT_ORDER).get();
 
+        // for case of date added type sorting
         if (sortType.equalsIgnoreCase(SortCommand.DATE_ADDED_SORT_TYPE_ARGUMENT)) {
+            // for case of ascending sort order
             if (sortOrder.equalsIgnoreCase(SortCommand.ASCENDING_SORT_ORDER_ARGUMENT)) {
                 return new SortCommand(SortCommand.COMPARATOR_SORT_PERSONS_BY_DATE_ADDED_ASCENDING);
+            // for case of descending sort order
             } else if (sortOrder.equalsIgnoreCase(SortCommand.DESCENDING_SORT_ORDER_ARGUMENT)) {
                 return new SortCommand(SortCommand.COMPARATOR_SORT_PERSONS_BY_DATE_ADDED_DESCENDING);
+            // invalid sort order
             } else {
                 throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
             }
+        // for case of name type sorting
         } else if (sortType.equalsIgnoreCase(SortCommand.NAME_SORT_TYPE_ARGUMENT)) {
+            // for case of ascending sort order
             if (sortOrder.equalsIgnoreCase(SortCommand.ASCENDING_SORT_ORDER_ARGUMENT)) {
                 return new SortCommand(SortCommand.COMPARATOR_SORT_PERSONS_BY_NAME_ASCENDING);
+            // for case of descending sort order
             } else if (sortOrder.equalsIgnoreCase(SortCommand.DESCENDING_SORT_ORDER_ARGUMENT)) {
                 return new SortCommand(SortCommand.COMPARATOR_SORT_PERSONS_BY_NAME_DESCENDING);
+            // invalid sort order
             } else {
                 throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
             }
+        // invalid sorting type
         } else {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
